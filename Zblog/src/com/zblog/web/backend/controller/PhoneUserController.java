a3dc55a1e403adb34c5eb836ac3f950caefcf8fe
leixun <leixun33@163.com>
2015-12-07 07:38:46 +0800

    sd

---
+++ Zblog/src/com/zblog/web/backend/controller/PhoneUserController.java
@@ -0,0 +1,42 @@
+/**
+ * 工程名: Zblog
+ * 文件名: PhoneUserController.java
+ * 包名: com.zblog.web.backend.controller
+ * 日期: 2015年12月6日上午8:53:35
+ * Copyright (c) 2015, 暴走兄弟 All Rights Reserved.
+ * 
+ * Mail: leixun33@163.com
+ * QQ: 378640336
+ *
+*/
+
+package com.zblog.web.backend.controller;
+/**
+ * 类名: PhoneUserController <br/>
+ * 功能: TODO 添加功能描述. <br/>
+ * 日期: 2015年12月6日 上午8:53:35 <br/>
+ *
+ * @author   leixun
+ * @version  	 
+ */
+public class PhoneUserController {
+	
+	// 客户端上传登陆启动等详细信息
+	public void postPhoneUserInfo(){
+		
+	}
+	
+	// 收藏信息
+	public void getFavorImg(){
+		
+	}
+	
+	// 获取app配置信息
+	public void getAppConfig(){
+		
+	}
+	
+	
+	
+}
+
