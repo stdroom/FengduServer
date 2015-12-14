1f87f03ab7d9dcb40909857a77500f20906d5b03
ammike <ammike@163.com>
2015-12-14 17:56:26 +0800

    test

--- Zblog/test/com/zblog/core/dal/mapper/PhoneUserMapperTest.java
+++ Zblog/test/com/zblog/core/dal/mapper/PhoneUserMapperTest.java
@@ -55,25 +55,26 @@ private SqlSessionFactory sqlSessionFactionFactory;
 		PhoneUserMapper userMapper = sqlSession.getMapper(PhoneUserMapper.class);
 		int i = 1;
 		PhoneUser phoneUser = new PhoneUser();
+		phoneUser.setIid(3);
 		phoneUser.setId("2");
 		phoneUser.setAppid(233);
 		phoneUser.setAppversion(233);
-		phoneUser.setCreateTime(new Date());
+		phoneUser.setDate(new Date());
 		phoneUser.setImei("imei");
 		phoneUser.setOsversion("osversion");
 		phoneUser.setLang("as");
 		phoneUser.setPhone_type("android");
 		phoneUser.setTimestamp("asd");
 		phoneUser.setClient_type("ad");
+		phoneUser.setHttpcount(23);
 		userMapper.insert(phoneUser);
-		PhoneUser user = userMapper.loadByPhoneUser(i);
+		PhoneUser user = userMapper.loadByPhoneUser(2);
 		System.out.println(user.getClient_type()
 				+"\n"+user.getImei()
 				+"\n"+user.getPhone_type()
 				+"\n"+user.getAppid()
 				);
 		assertEquals(user.getAppid(), new Integer(233));
-		
 	}
 //
 //	public void testListPageModelOfT() {
